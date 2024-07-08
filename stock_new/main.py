37bcc125e826a0ef5d266ac1d6148d0aa85da338
Sam Gonzalez <61301415+sammiebelle@users.noreply.github.com>
2024-07-08 11:36:08 -0600

    Add files via upload

---
+++ stock_new/main.py
@@ -0,0 +1,18 @@
+import requests
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+STOCK_API_KEY = "WQ95MZNW1XNW9V28"
+
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": STOCK_API_KEY,
+}
+
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
+print(response.json())
